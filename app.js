fa46f703bf4049e59dcc1cfd2b1d11dab12b1048
Collin D. Bevins <cbevins@montana.com>
2020-05-21 13:54:17 -0600

    replaced LoopIterator with generateArray()

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 import { BehavePlus } from './dist/bundle.esm.js'
-import { LoopIterator } from './src/dag/LoopIterator.js'
 
 const root = new BehavePlus.BpxDag('caseWise')
 console.log(`BehavePlus has ${root.node.map.size} Nodes`)
-
-const a = [...new LoopIterator(1, 10, 1)]
-console.log('LoopIterator(1, 10, 1) yields: ', a)
-const b = [...new LoopIterator(10, -2, 2)]
-console.log('LoopIterator(10, -2, 2) yields: ', b)
