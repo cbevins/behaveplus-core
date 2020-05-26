6bf05f3c7c58f54ecf0736c31fbea1fc2c846bee
Collin D. Bevins <cbevins@montana.com>
2020-05-25 21:19:24 -0600

    Multiple rollup builds outputs

--- rollup.config.js
+++ rollup.config.js
@@ -9,32 +9,70 @@ const bundle = `${dist}/${packageName}`
 
 // const production = !process.env.ROLLUP_WATCH
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: `${bundle}.esm.js`,
-      format: 'esm'
-    },
-    {
-      file: `${bundle}.esm.min.js`,
-      format: 'esm',
-      plugins: [terser()]
-    },
-    {
-      file: `${bundle}.cjs.js`,
-      format: 'cjs'
-    },
-    {
-      file: `${bundle}.cjs.min.js`,
-      format: 'cjs',
-      plugins: [terser()]
-    }
-  ],
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ]
-}
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: `${bundle}.esm.js`,
+        format: 'esm'
+      },
+      {
+        file: `${bundle}.esm.min.js`,
+        format: 'esm',
+        plugins: [terser()]
+      },
+      {
+        file: `${bundle}.cjs.js`,
+        format: 'cjs'
+      },
+      {
+        file: `${bundle}.cjs.min.js`,
+        format: 'cjs',
+        plugins: [terser()]
+      }
+    ],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ]
+  },
+  {
+    input: 'src/dag/index.js',
+    output: [
+      {
+        file: `${bundle}-dag.esm.js`,
+        format: 'esm'
+      }
+    ]
+  },
+  {
+    input: 'src/equations/index.js',
+    output: [
+      {
+        file: `${bundle}-equations.esm.js`,
+        format: 'esm'
+      }
+    ]
+  },
+  {
+    input: 'src/variants/index.js',
+    output: [
+      {
+        file: `${bundle}-variants.esm.js`,
+        format: 'esm'
+      }
+    ]
+  },
+  {
+    input: 'src/behaveplus/index.js',
+    output: [
+      {
+        file: `${bundle}-behaveplus.esm.js`,
+        format: 'esm'
+      }
+    ]
+  }
+]
